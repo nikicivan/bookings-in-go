a98f4b5ef8e378b81443490f5e3d346faf6c6a08
nikicivan <ivan.nikic@gmail.com>
2021-12-26 23:47:45 +0100

    added basic setup

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/nikicivan/bookings-in-go
+
+go 1.17
+
+require (
+	github.com/alexedwards/scs v1.4.1 // indirect
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/nikicivan/bookings-in-go/pkg/config"
+	"github.com/nikicivan/bookings-in-go/pkg/models"
+	"github.com/nikicivan/bookings-in-go/pkg/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new Repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again!"
+
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIp
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
